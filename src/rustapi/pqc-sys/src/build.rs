64dc00767ee1142c3619f4b4f56b2eb0e4a3d27c
Kris Kwiatkowski <contact@amongbytes.com>
2021-03-25 00:02:10 +0000

    adds rust binding

---
+++ src/rustapi/pqc-sys/src/build.rs
@@ -0,0 +1,34 @@
+extern crate cmake;
+use cmake::Config;
+extern crate bindgen;
+
+fn main() {
+	let dst = Config::new("../../../")
+		.profile("Release")
+		.very_verbose(true)
+		.build();
+
+	println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=dylib=pqc");
+    println!("cargo:rerun-if-changed=../../../capi/*,../../../kem/*,../../../sign/*,../../../../public/pqc/pqc.h");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("../../../public/pqc/pqc.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Add "Default" whenever possible
+        .derive_default(true)
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+    bindings
+    	.write_to_file("src/bindings.rs")
+    	.expect("Couldn't write bindings");
+}
