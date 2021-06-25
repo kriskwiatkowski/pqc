caa97d8dfb6084748cded42f4e0bac1839a9dd75
Kris Kwiatkowski <contact@amongbytes.com>
2021-06-25 09:05:54 +0100

    Test CT sanitizer and CTGRIND functionality

--- test/ct.cpp
+++ test/ct.cpp
@@ -1,23 +1,70 @@
-#include <algorithm>
-#include <vector>
+// Those tests work only with Clang and Memory Sanitizer
+
 #include <gtest/gtest.h>
-#include <pqc/pqc.h>
+#include <common/ct_check.h>
+#include <stdio.h>
+// tests from https://github.com/agl/ctgrind/blob/master/test.c
 
-// #ifdef VALGRIND
-// #include <valgrind/valgrind.h>
-// #include <valgrind/memcheck.h>
-// #define POISON(p,sz) VALGRIND_MAKE_MEM_UNDEFINED(p,sz)
-// #endif
+void nothing(void) {
+  printf("exiting...");
+}
 
-#ifdef PQC_MEMSAN
-#include <sanitizer/msan_interface.h>
-#define POISON(p,sz) __msan_poison(p,sz)
-#endif
+TEST(ConstantTime, CtGrind_Negative) {
+    unsigned char a[16], b[16];
+    unsigned i;
+    memset(a, 42, 16);
+    memset(b, 42, 16);
+
+    CT_DYE(a, 16);
+    for (i = 0; i < 16; i++) {
+        CT_EXPECT_UMR();
+        if (a[i] != b[i]) {
+            break;
+        }
+        CT_REQUIRE_UMR();
+    }
+
+    CT_PURIFY(a, 16);
+    // Ensure buffers are not optimized-out
+    ASSERT_EQ(a[0], b[0]);
+}
+
+TEST(ConstantTime, CtGrind_Positive_NoAccess) {
+    unsigned i;
+    char result = 0;
+    unsigned char a[16], b[16];
+    memset(a, 42, sizeof(a));
+    memset(b, 42, sizeof(b));
+
+    CT_DYE(a, 16);
+
+    for (i = 0; i < 16; i++) {
+        result |= a[i] ^ b[i];
+    }
+    CT_PURIFY(a, 16);
+
+    // Purify result, to allow check that otherwise
+    // would be not constant-time.
+    CT_PURIFY(&result, 1);
+    ASSERT_EQ(result, 0);
+}
 
 
-TEST(ConstantTime, Poisonner_Basic) {
-	unsigned char x[8] = {0};
-//gi	POISON(x, 4);
-	if(x[5]) x[6] = x[5];
-	//UNPOISON(x, 4);
+TEST(ConstantTime, CtGrind_Negative_UseSecretAsIndex) {
+    static const unsigned char tab[2] = {1, 0};
+    unsigned char a[16];
+    unsigned char result;
+    memset(a, 42, sizeof(a));
+
+    CT_DYE(a, 16);
+
+    CT_EXPECT_UMR();
+    result = tab[a[0] & 1];
+    CT_REQUIRE_UMR();
+
+    CT_PURIFY(a, 16);
+
+    // Ensure variables are not optimized-out
+    CT_PURIFY(&result, 1);
+    ASSERT_EQ(result, 1);
 }
